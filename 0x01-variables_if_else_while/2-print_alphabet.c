5e5736fa0f6de827a41740aec625abd57dacfab2
Kiprop <kipropa77@gmail.com>
2023-03-17 13:38:55 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return 0-Success
+ *Return 0-Success
  */
-
-
-
 
 int main(void)
 /*main program code that prints the alphabet in lowercase, followed by a new line*/
