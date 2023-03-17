a268976021563f4b762e5a557810421f0e69d3c6
Kiprop <kipropa77@gmail.com>
2023-03-17 07:35:53 -0700

    d

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Only function printing the random numbers
+ *
+ * Return: 0 is success
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	switch (n < 0 ? -1 : (n > 0 ? 1 : 0)) {
+    case -1:
+        printf("%i is negative\n", n);
+        break;
+    case 0:
+        printf("%i is zero\n", n);
+        break;
+    case 1:
+        printf("%i is positive\n", n);
+        break;
+}	return (0);
+}
