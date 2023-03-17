b1cba843d594c087e1654be390e9a3fbc94e7262
Kiprop <kipropa77@gmail.com>
2023-03-17 08:06:25 -0700

    d

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,21 +12,20 @@ int main(void)
 {
 int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
 switch (n < 0 ? -1 : (n > 0 ? 1 : 0)) 
 {
-	case -1:
-            printf("%i is negative\n", n);
-            break;
-        case 0:
-            printf("%i is zero\n", n);
-            break;
-        case 1:
-            printf("%i is positive\n", n);
-            break;
-    }
-
-    return (0);
+case -1:
+printf("%i is negative\n", n);
+break;
+case 0:
+printf("%i is zero\n", n);
+break;
+case 1:
+printf("%i is positive\n", n);
+break;
+}
+return (0);
 }
