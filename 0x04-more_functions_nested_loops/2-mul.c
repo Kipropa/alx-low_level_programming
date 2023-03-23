9ca7b384cc43c38b7f1e42a937cc8c721553004f
Kiprop <kipropa77@gmail.com>
2023-03-23 14:58:31 -0700

    product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *prod-of a and b
+ *a and b are the numbers to be multiplied 
+ * the program returns the product of a  and b 
+ */
+
+int (int a, int b)
+{
+return (a * b);
+}
