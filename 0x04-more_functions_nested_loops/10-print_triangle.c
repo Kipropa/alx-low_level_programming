5708e61591530fa6753c34dfc58e20e0e317d182
Kiprop <kipropa77@gmail.com>
2023-03-24 11:41:57 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,16 +9,26 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+    int i, j;
+    int num_spaces = size - 1;
+    int num_hashes = 1;
+    int row_length = size;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 1; j < (size - i); j++)
-			putchar(' ');
-		for (j--; j < size; j++)
-			putchar(35);
-		if (i < (size - 1))
-			putchar('\n');
-	}
-	putchar('\n');
+    for (i = 0; i < row_length * size; i++) {
+        if (i % row_length == 0 && i != 0) {
+            putchar('\n');
+            num_spaces--;
+            num_hashes++;
+            row_length--;
+        }
+
+        if (num_spaces > 0) {
+            putchar(' ');
+            num_spaces--;
+        } else {
+            putchar('#');
+            num_hashes++;
+        }
+    }
+    putchar('\n');
 }
