713b7715877787cb51493fce622fe225d7e8eb5c
Kiprop <kipropa77@gmail.com>
2023-04-05 15:43:23 -0700

    more recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,27 +1,24 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings can be considered identical
- * @s1: first string to compare
- * @s2: second string to compare, can contain the special character *
+ * wildcmp - compares two strings and returns 1 if the strings can be
+ *           considered identical, otherwise returns 0.
+ * @s1: the first string
+ * @s2: the second string that can contain the special character '*'
  *
- * Return: 1 if the strings can be considered identical, otherwise 0
+ * Return: 1 if the strings are identical, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-        return (1);
-
-    if (*s2 == '*')
-    {
-        if (*s1 == '\0')
-            return (wildcmp(s1, s2 + 1));
-
-        return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-    }
-
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
-
-    return (0);
+	if (*s1 == '\0' && *s2 == '\0') /* If both strings are empty */
+		return (1);
+	if (*s1 == *s2) /* If the characters match */
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && *s1 == '\0') /* If s2 is all '*' */
+		return (wildcmp(s1, s2 + 1));
+	if (*s2 == '*' && (*(s2 + 1) == '*' || *(s1 + 1) != '\0'))
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	if (*s2 == '*' && *(s2 + 1) == '\0') /* If s2 ends in '*' */
+		return (1);
+	return (0); /* If none of the conditions are met */
 }
